c030debed8e88c4f7b8b918094a0490a4d12996c
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-21 17:57:34 -0300

    Criando camada Data

---
+++ VagasAPI/Data/AppDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VagasAPI.Model;
+
+namespace VagasAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Job>().ToTable("tb_jobs");
+        }
+
+        public DbSet<Job> Jobs { get; set; } = null!;
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var insertedEntries = this.ChangeTracker.Entries()
+                                   .Where(x => x.State == EntityState.Added)
+                                   .Select(x => x.Entity);
+
+            foreach (var insertedEntry in insertedEntries)
+            {
+                //Se uma propriedade da Classe Auditable estiver sendo criada. 
+                if (insertedEntry is Auditable auditableEntity)
+                {
+                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
+                }
+            }
+
+            var modifiedEntries = ChangeTracker.Entries()
+                       .Where(x => x.State == EntityState.Modified)
+                       .Select(x => x.Entity);
+
+            foreach (var modifiedEntry in modifiedEntries)
+            {
+                //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
+                if (modifiedEntry is Auditable auditableEntity)
+                {
+                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
+
