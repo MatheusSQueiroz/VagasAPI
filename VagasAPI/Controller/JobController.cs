64a2039415bfb9284a2579744d16b53ddc4dc057
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-21 18:19:08 -0300

    Configurando service

--- VagasAPI/Controller/JobController.cs
+++ VagasAPI/Controller/JobController.cs
@@ -2,12 +2,22 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using VagasAPI.Model;
 using VagasAPI.Service;
 
 namespace VagasAPI.Controller
 {
-    public class JobController
+    [Route("~/jobs")]
+    [ApiController]
+    public class JobController : ControllerBase
     {
-        
+        private readonly IJobService _jobService;
+        private readonly IValidator<Job> _jobValidator;
+
+        public JobController(IJobService jobService, IValidator<Job> jobValidator)
+        {
+            _jobService = jobService;
+            _jobValidator = jobValidator;
+        }
     }
 }
